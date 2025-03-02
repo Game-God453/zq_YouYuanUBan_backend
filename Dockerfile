4659c96f5e863272a06cba4bb1cd5efa610afc8c
Game_God <3363808215@qq.com>
2025-03-02 17:15:44 +0800

    配置了项目部署相关文件

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Django
+FROM python:3.12-buster
+#ENV PYTHONUNBUFFERED=1
+RUN sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+RUN apt-get update && apt-get install -y vim
+WORKDIR /etc/uwsgi/django
+RUN python3 -m pip install uwsgi uwsgi-tools -i https://pypi.tuna.tsinghua.edu.cn/simple/
+ADD requirements.txt /etc/uwsgi/django/requirements.txt
+RUN python3 -m pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
+#ADD uwsgi.ini uwsgi.ini
+#ADD . /etc/uwsgi/django
+#EXPOSE 8086
+#CMD uwsgi --ini /etc/uwsgi/django/uwsgi.ini
+#采用卷挂载，在docker-compose.yml文件中配置
--- YouYuanUBan_backend/settings.py
+++ YouYuanUBan_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%=xsn7!+bimxe2h78x12_t2$o7n#hj3!j3j#)$+hd48x4_0rr_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -102,7 +102,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'youyuanuban',
         'USER': 'root',
-        'PASSWORD': 'Fytyg20051216',
+        'PASSWORD': 'zq123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
@@ -159,7 +159,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
